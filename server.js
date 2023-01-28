9c4019bb675dbf123f7918a20cac794a3ae5bf9f
Annie Vo <avo2@unc.edu>
2023-01-28 18:52:13 -0500

    server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var parseArgs  = require('minimist')(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
-const port = args.port || 3000
+const port = parseArgs.port || 3000
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
